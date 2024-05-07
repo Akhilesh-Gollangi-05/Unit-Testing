cb57a28b557ca531c8740484bd913533fa7064b0
Akhilesh-Gollangi-05 <169146535+Akhilesh-Gollangi-05@users.noreply.github.com>
2024-05-07 15:33:49 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,20 @@
 import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        char[] input={'a','b','c','d','e'};
+        char[] input={'a','b','c','D','e'};
         convertToUppercase(input);
         System.out.println(Arrays.toString(input));
     }
 
-    private static void convertToUppercase(char[] name) {
-        for(int i = 0, e = name.length; i<e; ++i){
-            if(Character.isLetter(name[i]) && Character.isLowerCase(name[i])) {
-                name[i] = (char) (name[i] - 32);
+    private static void convertToUppercase(char[] input) {
+        for(int i = 0, e = input.length; i<e; ++i){
+            if(Character.isLetter(input[i]) && Character.isLowerCase(input[i])) {
+                input[i] = (char) (input[i] - 32);
             }
         }
     }
 }
+
+
 
