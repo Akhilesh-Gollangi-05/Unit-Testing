a18860540215473e3ef605482985650e8ef95c11
Akhilesh-Gollangi-05 <169146535+Akhilesh-Gollangi-05@users.noreply.github.com>
2024-05-07 15:57:41 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,14 +7,13 @@ public class Main  {
     private static String covertToUppercase(String input) {
         char[] inputArray= input.toCharArray();
         for(int i=0,e=inputArray.length;i<e;++i){
-            input ="";
             if(Character.isLetter(inputArray[i]) && Character.isLowerCase(inputArray[i])){
                 inputArray[i]=(char)(inputArray[i]-32);
             }
         }
-        String result =new String(inputArray);
-        return result;
+        return new String(inputArray);
     }
 }
+
 
 
